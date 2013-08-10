2feb21735d46674ebdded448e07ea2bc0b007ca1
Yevgen Iliashchienko <yevgen.iliashchienko@yahoo.com>
2013-08-10 23:50:20 +0300

    Создал файл контроллера.

---
+++ GitWelcome/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  GitWelcome
+//
+//  Created by Yevgen Iliashchienko on 8/10/13.
+//  Copyright (c) 2013 Nikita Severinov. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end

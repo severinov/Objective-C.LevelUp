d6bc5a494b8fd8fe386d4be9075171b91d95a071
Nikita Severinov <nikita.severinov@gmail.com>
2013-08-11 01:25:24 +0400

    Видоизменение

--- GitWelcome/ViewController.h
+++ GitWelcome/ViewController.h
@@ -1,15 +1,5 @@
-//
-//  ViewController.h
-//  GitWelcome
-//
-//  Created by Severinov N.A. on 07.08.13.
-//  Copyright (c) 2013 Nikita Severinov. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-@property (weak, nonatomic) IBOutlet UILabel *display;
 
 @end
--- GitWelcome/ViewController.m
+++ GitWelcome/ViewController.m
@@ -2,48 +2,10 @@
 #import "ViewController.h"
 
 @interface ViewController()
-@property (nonatomic) BOOL areWeInTheMiddleOfEnteringNumber;
+
 @end
 
 @implementation ViewController
 
-@synthesize display = _display;
-
-- (IBAction)digitPressed:(id)sender
-{
-    NSLog(@"digit pressed = %@", [sender currentTitle]);
-    
-    NSString *digit = [sender currentTitle];
-    
-    if (self.areWeInTheMiddleOfEnteringNumber)
-        self.display.text = [self.display.text stringByAppendingString:digit];
-    else
-    {
-        if ([digit isEqualToString:@"0"])
-            return; //Several zero at begining is not allowed
-        else if ([digit isEqualToString:@"."]) //Zero before point. 
-        {
-            self.display.text = [self.display.text stringByAppendingString:digit];
-            return;
-        }
-        else self.display.text = digit;
-        
-        self.areWeInTheMiddleOfEnteringNumber = YES;
-    }
-}
-
-- (IBAction)operationPressed:(id)sender
-{
-}
-
-- (IBAction)enterPressed
-{
-}
-
-- (IBAction)clearPressed
-{
-    self.areWeInTheMiddleOfEnteringNumber = NO;
-    self.display.text = @"0";
-}
 
 @end

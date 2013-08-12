efb876c261666705ae668af4ad8e93f664f76a43
AlexanderNaumov <haymob@me.com>
2013-08-12 05:13:27 +0400

    новая кнопка

---
+++ GitWelcome/ButtonView.h
@@ -0,0 +1,13 @@
+//
+//  ButtonView.h
+//  testCLC
+//
+//  Created by Alexander on 11.08.13.
+//  Copyright (c) 2013 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ButtonView : UIButton
+
+@end
